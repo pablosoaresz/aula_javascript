acf604426427342a7c8a05a0591b1a42bc1a38e4
pablosoaresz <pabloetf@gmail.com>
2020-03-02 10:20:29 -0400

    Finalizando introdução ao JS

--- js/main.js
+++ js/main.js
@@ -1,8 +1,69 @@
+function clicked() {
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar </b>"
+        //console.log(document.getElementById("agradecimento"))
+        //alert("Obrigado por clicar")
+}
+
+function redirecionar() {
+    window.open("https://www.google.com.br") //abre em nova janela
+    window.location.href = "https://www.uol.com.br" //Abre o endereço na mesma janela
+}
+
+function trocar(elemento) {
+
+    elemento.innerHTML = "Mouse passou!"
+        //document.getElementById("mousemove").innerHTML = "Mouse passou, Obrigado"
+        //alert("Torcar texto")
+}
+
+function voltar(elemento) {
+    elemento.innerHTML = "Passe o mouse aqui"
+        //document.getElementById("mousemove").innerHTML = "Passe o mouse aqui.   "
+}
+
+function load() {
+    alert("Pagina carregou!")
+}
+
+function funcaoChange(elemento) {
+    console.log(elemento.value)
+}
+
+/*function soma(n1, n2) {
+    return n1 + n2
+}
+
+function setReplace(frase, nome, novo_nome) {
+    return frase.replace(nome, novo_nome)
+}
+alert(soma(5, 10))
+alert(setReplace("Vai VGê", "VGê", "Cuiabá"))
+*/
+
+/*
+var validar; //variável global
+function validaIdade(idade) {
+    var validar; //variável local
+    if (idade >= 18) {
+        validar = true
+
+    } else {
+        validar = false
+    }
+    return validar
+}
+
+var idade = prompt("Qual sua idade?")
+console.log(validaIdade(idade))
+*/
+
+/*
 var d = new Date()
 alert(d.getMonth() + 1) //Adicionado o +1 para mostrar o mes atual
 alert(d.getDay())
 alert(d.getFullYear())
 alert(d.getHours())
+*/
 
 /*
 var count;
