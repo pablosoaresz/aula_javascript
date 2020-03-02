6c9df6cfb756d118b64a37120fab4fce5c57af13
pablosoaresz <pabloetf@gmail.com>
2020-03-02 00:43:02 -0400

    Curso Basico de JS

---
+++ js/main.js
@@ -0,0 +1,66 @@
+var d = new Date()
+alert(d.getMonth() + 1) //Adicionado o +1 para mostrar o mes atual
+alert(d.getDay())
+alert(d.getFullYear())
+alert(d.getHours())
+
+/*
+var count;
+for (count = 0; count <= 5; count++) {
+    alert(count)
+}
+*/
+
+/*
+var count = 0
+
+while (count < 5) {
+    console.log(count)
+    alert(count)
+    count++
+}
+*/
+
+/*
+var idade = prompt("Qual a sua idade")
+    //var idade = 18
+if (idade >= 18) {
+    alert("Maior de idade")
+} else {
+    alert("Menor de idade")
+}
+*/
+
+/*
+var nome = "Pablo Bachega Soares"
+var idade = 35
+var idade2 = 10
+n1 = 5
+n2 = 3
+var frase = "Cuiab'a 'e o melhor time do mundo!"
+    //alert(nome + " tem " + idade);
+    //alert(idade + idade2)
+console.log(nome)
+console.log(idade + idade2)
+console.log(frase.replace("Cuiab'a", "V'arzea Grande"))
+console.log(frase.toUpperCase())
+console.log(frase.toLowerCase())
+console.log(n1 * n2)
+    //console.log(n1 / n44444)
+
+var lista = ["Ma'c~a", "P^era", "Laranja"];
+lista.push("Uva")
+    //lista.pop()
+console.log(lista.reverse());
+console.log(lista[0])
+console.log(lista.toString()[0])
+console.log(lista.join(" |-| "))
+
+var fruta = { nome: "maca", cor: "vermelha" }
+console.log(fruta.nome)
+alert(fruta.cor)
+
+var frutas = [{ nome: "maca", cor: "vermelhra" }, { nome: "uva", cor: "roxa" }]
+console.log(frutas)
+alert(frutas[1].nome)
+*/
